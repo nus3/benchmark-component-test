6f0b40c99cb41869f121b3f20421554e2604e045
nus3 <hadayota33@gmail.com>
2024-09-05 18:43:12 +0900

    update bench script

--- scripts/benchmark.ts
+++ scripts/benchmark.ts
@@ -1,36 +1,27 @@
-import { Bench } from "tinybench";
 import { exec } from "node:child_process";
+import { performance } from "node:perf_hooks";
+import { promisify } from "node:util";
 
-const roundToFourthDecimal = (num: number) => {
-  return Math.round(num * 10000) / 10000;
+const execAsync = promisify(exec);
+
+const runScript = async (script: string, label: string) => {
+  const start = performance.now();
+  await execAsync(script);
+  const end = performance.now();
+  const duration = (end - start) / 1000;
+  return { name: label, duration: `${duration.toFixed(2)}s` };
 };
 
-const bench = new Bench();
+const main = async () => {
+  const scripts = [
+    { script: "pnpm --filter react-app run test-ci", label: "vitest" },
+    { script: "pnpm --filter react-app run test-ci", label: "vitest2" },
+  ];
 
-// TODO: なんか無限にtimestampが生成されるかも。バグってそう
-// vitestでテスト実行したら終わるようなオプションが確かあったはず
-bench.add("vitest", async () => {
-  await exec("pnpm --filter react-app run test-ci");
-});
+  const results = await Promise.all(
+    scripts.map(({ script, label }) => runScript(script, label)),
+  );
+  console.table(results);
+};
 
-await bench.warmup(); // make results more reliable, ref: https://github.com/tinylibs/tinybench/pull/50
-await bench.run();
-
-console.table(
-  bench.table((task) => ({
-    "Task Name": task.name,
-    // the number of operations per second
-    // 1秒間に何回、登録されたタスクが何回実行されるか？
-    hz: roundToFourthDecimal(task.result.hz),
-    min: roundToFourthDecimal(task.result.min),
-    max: roundToFourthDecimal(task.result.max),
-    // samples mean/average (estimate of the population mean)
-    mean: roundToFourthDecimal(task.result.mean),
-    // p75 percentile
-    p75: roundToFourthDecimal(task.result.p75),
-    p99: roundToFourthDecimal(task.result.p99),
-    // relative margin of error
-    rme: roundToFourthDecimal(task.result.rme),
-    samples: roundToFourthDecimal(task.result.samples.length),
-  })),
-);
+main();
