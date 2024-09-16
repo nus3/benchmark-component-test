6b8eebceedf502236cab929b589d12f47ae6d955
nus3 <hadayota33@gmail.com>
2024-09-16 13:15:06 +0900

    remove port

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,9 +3,6 @@ import { defineWorkspace } from "vitest/config";
 export default defineWorkspace([
   {
     extends: "vite.config.ts",
-    server: {
-      port: 3001,
-    },
     test: {
       name: "jsdom",
       environment: "jsdom",
@@ -16,9 +13,6 @@ export default defineWorkspace([
   },
   {
     extends: "vite.config.ts",
-    server: {
-      port: 3002,
-    },
     test: {
       name: "playwright",
       include: ["./src/test/vitest/**/*.test.{ts,tsx}"],
@@ -34,9 +28,6 @@ export default defineWorkspace([
   },
   {
     extends: "vite.config.ts",
-    server: {
-      port: 3003,
-    },
     test: {
       name: "webdriverio",
       include: ["./src/test/vitest/**/*.test.{ts,tsx}"],
