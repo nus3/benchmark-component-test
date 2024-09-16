5649738be8acaf8fd0084f98c9f3a28fd7e119de
nus3 <hadayota33@gmail.com>
2024-09-16 10:33:48 +0900

    add vitest playwright test

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -5,11 +5,25 @@ export default defineWorkspace([
     extends: "vite.config.ts",
     test: {
       name: "jsdom",
-      globals: true,
       environment: "jsdom",
       setupFiles: "./src/test/jsdom/setup.ts",
       css: true,
       include: ["./src/test/jsdom/**/*.test.{ts,tsx}"],
+    },
+  },
+  {
+    extends: "vite.config.ts",
+    test: {
+      name: "playwright",
+      include: ["./src/test/jsdom/**/*.test.{ts,tsx}"],
+      browser: {
+        enabled: true,
+        headless: true,
+        name: "chromium",
+        provider: "playwright",
+        // https://playwright.dev
+        providerOptions: {},
+      },
     },
   },
 ]);
